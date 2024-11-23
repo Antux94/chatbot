bdfad437e02228c49572f001cabde3583a409a81
Antux94 <153137280+Antux94@users.noreply.github.com>
2024-11-23 22:36:18 +0000

    ajuste ubicacion shell

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 echo hello
 
 
-cd /mount/src/chatbot
+#cd /mount/src/chatbot
 
 pwd
 
-export JAVA_HOME=jdk1.8.0_202
+export JAVA_HOME=/mount/src/chatbot/jdk1.8.0_202
 export PATH=$JAVA_HOME/bin:$PATH
 
-export M2_HOME=apache-maven-3.5.3
+export M2_HOME=/mount/src/chatbot/apache-maven-3.5.3
 export PATH=$M2_HOME/bin:$PATH
 
 echo $JAVA_HOME
