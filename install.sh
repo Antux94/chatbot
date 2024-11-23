77059204ff7d429b274ee4520b03912942a469dd
Antux94 <153137280+Antux94@users.noreply.github.com>
2024-11-23 22:18:15 +0000

    Ajuste shell

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo hello
 
-cd /workspaces/chatbot
+
+#cd /workspaces/chatbot
 pwd
 
 export JAVA_HOME=jdk1.8.0_202
@@ -8,5 +9,8 @@ export PATH=$JAVA_HOME/bin:$PATH
 
 export M2_HOME=apache-maven-3.5.3
 export PATH=$M2_HOME/bin:$PATH
+
+echo $JAVA_HOME
+echo $M2_HOME
 
 mvn -version
