7105990123b498c2bfa6cb91e280622c634683ac
Antux94 <153137280+Antux94@users.noreply.github.com>
2024-11-23 23:59:27 +0000

    intalacion java 8

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ echo SHELL---------------------------------------------------------------------
 pwd
 
 
-wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+
+
 
 
 
