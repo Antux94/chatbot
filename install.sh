8c92f0d642826081534156e6650bfd33a96b1332
Antux94 <153137280+Antux94@users.noreply.github.com>
2024-11-23 22:52:19 +0000

    java version

--- install.sh
+++ install.sh
@@ -7,8 +7,11 @@ pwd
 
 ls
 
-#export JAVA_HOME=/mount/src/chatbot/jdk1.8.0_202
-#export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME=jdk1.8.0_202
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo JAVA VERSION
+java -version
 
 #export M2_HOME=/mount/src/chatbot/apache-maven-3.5.3
 #export PATH=$M2_HOME/bin:$PATH
