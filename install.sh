fd24302868b26283d117cbd079f5b18262a1e823
Antux94 <153137280+Antux94@users.noreply.github.com>
2024-11-24 01:57:17 +0000

    java 11

--- install.sh
+++ install.sh
@@ -3,11 +3,6 @@ echo SHELL---------------------------------------------------------------------
 
 #cd /mount/src/chatbot
 
-wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
-tar -xvzf openjdk-11.0.2_linux-x64_bin.tar.gz
-
-
-
 
 pwd
 ls
