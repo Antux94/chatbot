11dc62cb99398a3e0bc88e8c080eb0db0e5912ac
Antux94 <153137280+Antux94@users.noreply.github.com>
2024-11-24 01:52:33 +0000

    se coloca instalacion para docker java 11 openJDK

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@ echo SHELL---------------------------------------------------------------------
 
 
 #cd /mount/src/chatbot
+
+wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
+tar -xvzf openjdk-11.0.2_linux-x64_bin.tar.gz
+
+
+
+
 pwd
 ls
 
